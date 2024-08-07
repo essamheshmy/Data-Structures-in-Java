10049b8c1d9c05d516f3deff88ac21e093dbde6d
essam <essamheshmey>
2024-08-07 16:18:21 +0300

    no commit

---
+++ Data Structures in Java/src/Data_Structures_Files/AlternativeJavaDataStructures/_LinkedHashMap.java
@@ -0,0 +1,14 @@
+package Data_Structures_Files.AlternativeJavaDataStructures;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class _LinkedHashMap {
+    public static void LinkedHashMap(){
+        Map<String, String> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put("Language", "Java");
+        linkedHashMap.put("Version", "11");
+        System.out.println(linkedHashMap);
+
+    }
+}
---
+++ Data Structures in Java/src/Data_Structures_Files/AlternativeJavaDataStructures/_Vector.java
@@ -0,0 +1,15 @@
+package Data_Structures_Files.AlternativeJavaDataStructures;
+
+import java.util.List;
+import java.util.Vector;
+
+public class _Vector {
+    public static void vector(){
+        List<String> vector = new Vector<>();
+        vector.add("Hello");
+        vector.add("Java");
+        vector.add("World");
+        System.out.println(vector);
+
+    }
+}
